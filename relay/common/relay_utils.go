a4d2f5a030c0d1d5be87605eb6b096f71e36c22d
xlingoo <mahaonan1@hotmail.com>
2024-11-22 13:36:55 +0800

    Add files via upload

---
+++ relay/common/relay_utils.go
@@ -0,0 +1,36 @@
+package common
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"one-api/common"
+	"strings"
+)
+
+var StopFinishReason = "stop"
+
+func GetFullRequestURL(baseURL string, requestURL string, channelType int) string {
+	fullRequestURL := fmt.Sprintf("%s%s", baseURL, requestURL)
+
+	if strings.HasPrefix(baseURL, "https://gateway.ai.cloudflare.com") {
+		switch channelType {
+		case common.ChannelTypeOpenAI:
+			fullRequestURL = fmt.Sprintf("%s%s", baseURL, strings.TrimPrefix(requestURL, "/v1"))
+		case common.ChannelTypeAzure:
+			fullRequestURL = fmt.Sprintf("%s%s", baseURL, strings.TrimPrefix(requestURL, "/openai/deployments"))
+		}
+	}
+	return fullRequestURL
+}
+
+func GetAPIVersion(c *gin.Context) string {
+	query := c.Request.URL.Query()
+	apiVersion := query.Get("api-version")
+	if apiVersion == "" {
+		apiVersion = c.GetString("api_version")
+	}
+	return apiVersion
+}
